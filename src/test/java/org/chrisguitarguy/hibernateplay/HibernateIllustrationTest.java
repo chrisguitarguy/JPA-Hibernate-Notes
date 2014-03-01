d348e63e6c9f7f54dadfb0470ba5eae0200b90a7
Christopher Davis <cdavis9999@gmail.com>
2014-03-01 13:20:00 -0500

    Start with an author test

--- src/test/java/org/chrisguitarguy/hibernateplay/HibernateIllustrationTest.java
+++ src/test/java/org/chrisguitarguy/hibernateplay/HibernateIllustrationTest.java
@@ -1,9 +1,12 @@
 package org.chrisguitarguy.hibernateplay;
 
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -23,6 +26,22 @@ public class HibernateIllustrationTest {
     }
 
     @Test
-    public void testStuff() throws Exception {
+    public void testSaveGetAuthors() throws Exception {
+        EntityManager em = emFactory.createEntityManager();
+        em.getTransaction().begin();
+        em.persist(new Author("A", "Person"));
+        em.persist(new Author("Another", "Person"));
+        em.getTransaction().commit();
+        em.close();
+
+        em = emFactory.createEntityManager();
+        em.getTransaction().begin();
+        Query q = em.createQuery("from Author", Author.class);
+        List<Author> result = q.getResultList();
+        for (Author a : result) {
+            Assert.assertNotNull(a.getFirstName());
+        }
+        em.getTransaction().commit();
+        em.close()
     }
 }
